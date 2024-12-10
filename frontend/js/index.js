950f6dc5c7c33e65201b6712089b60b5eb16ee0a
Elliot <elliot@madebyverse.com>
2024-12-10 15:22:13 +1000

    form styling

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -36,16 +36,71 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       `);
 
+      sheet.insertRule(`
+        div[class*='_formFieldContainer'] {
+          position: relative !important;
+          margin-bottom: 1rem !important;
+          width: 100% !important;
+        }
+      `);
+
       // Base input styles
       sheet.insertRule(`
-        input {
+        input:not([type="radio"]) {
           outline: none;
           appearance: none;
           width: 100%;
           background-color: transparent;
-          padding: 1rem;
+          padding: 1rem 0;
           border: none !important;
-          border-bottom: 1px solid #241917 !important;
+          border-bottom: 1px solid rgba(36, 25, 23, 0.1) !important;
+          transition: all 0.5s ease-in-out !important;
+        }
+      `);
+
+      // Add position relative to input container
+      sheet.insertRule(`
+        div[class*='_inputField'] {
+          position: relative !important;
+          margin-top: 1rem !important;
+        }
+      `);
+
+      // Floating label styles
+      sheet.insertRule(`
+        label {
+          font-family: sans-serif !important;
+          text-transform: uppercase !important;
+          font-size: 0.75rem !important;
+          color: rgba(36, 25, 23, 0.6) !important;
+        }
+      `);
+
+      // Floating label styles
+      sheet.insertRule(`
+        label:not([class*='_radioField']) {
+          position: absolute !important;
+          left: 0;
+          top: 1rem ;
+          transition: all 0.2s ease-out !important;
+          pointer-events: none !important;
+          color: rgba(36, 25, 23, 0.6) !important;
+        }
+      `);
+
+      // Label float on input focus or when input has value
+      sheet.insertRule(`
+        label[class*='_inputFilled'] {
+           top: 0rem !important;
+          font-size: 0.75rem !important;
+          color: rgba(36, 25, 23, 1) !important;
+        }
+      `);
+
+      // Add placeholder to ensure label moves when there's content
+      sheet.insertRule(`
+        input::placeholder {
+          color: transparent !important;
         }
       `);
 
@@ -68,9 +123,11 @@ document.addEventListener("DOMContentLoaded", () => {
       // Textarea styles
       sheet.insertRule(`
         textarea {
+          outline: none !important;
           background-color: transparent !important;
-          border-bottom: 1px solid #C3C2BC !important;
-          padding: 1rem !important;
+          border: none !important;
+          border-bottom: 1px solid rgba(36, 25, 23, 0.1) !important;
+          padding: 1rem 0 !important;
           width: 100% !important;
           appearance: none !important;
         }
@@ -79,36 +136,161 @@ document.addEventListener("DOMContentLoaded", () => {
       // Radio input styles
       sheet.insertRule(`
         input[type="radio"] {
+          display: block !important;
           appearance: none !important;
           width: 0.5rem !important;
-          height: 1rem !important;
-          border-radius: 9999px !important;
-          border: 1px solid #C3C2BC !important;
+          height: 0.5rem !important;
+          border-radius: 0 !important;
+          border: 1px solid rgba(36, 25, 23, 0.1) !important;
+        }
+      `);
+
+      // Add radio input checked state
+      sheet.insertRule(`
+        input[type="radio"]:checked {
+          background-color: #241917 !important;
+          border-color: #241917 !important;
         }
       `);
 
       sheet.insertRule(`
-        button {
+        button:not([class*='_selectToggle']) {
+          margin-top: 3rem !important;
           appearance: none !important;
-          background-color: #241917 !important;
-          color: #fff !important;
+          background-color: transparent !important;
+          color: #241917 !important;
           display: flex !important;
           align-items: center !important;
           justify-content: center !important;
-
+          padding: 1rem !important;
+          width: 100% !important;
+          border: 1px solid rgba(36, 25, 23, 0.1) !important;
+          border-radius: 0 !important;
+          cursor: pointer !important;
+          transition: all 0.5s ease-in-out !important;
         }
         `)
+        sheet.insertRule(`
+          button:not([class*='_selectToggle']):hover {
+            border: 1px solid rgba(36, 25, 23, 1) !important;
+          }
+          `)
+
+
 
       sheet.insertRule(`
-        label {
-          color: #C3C2BC !important;
+        fieldset {
+          appearance: none !important;
+          border: none !important;
+          margin-bottom: 1rem !important;
+          display: flex !important;
+          flex-wrap: wrap !important;
+          gap: 0.5rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        legend {
+          font-family: sans-serif !important;
+          text-transform: uppercase !important;
+          font-size: 0.75rem !important;
+          color: rgba(36, 25, 23, 0.6) !important;
+          margin-bottom: 1rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        label[class*='_radioField'] {
+          appearance: none !important;
+          border: none !important;
+          display: flex !important;
+          align-items: center !important;
+          gap: 0.5rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        div[class*='_horizontalLayout'] {
+          display: flex !important;
+          flex-wrap: wrap !important;
+          align-items: center !important;
+          gap: 0.5rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        div[class*='_formPhoneInputContainer'] {
+          display: flex !important;
+          align-items: center !important;
+          gap: 0.5rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        button[class*='_selectToggle'] {
+          position: relative !important;
+          appearance: none !important;
+          background-color: transparent !important;
+          color: #241917 !important;
+          display: flex !important;
+          align-items: center !important;
+          justify-content: center !important;
+          padding: 1rem !important;
+          width: 100% !important;
+          border: none !important;
+          border-bottom: 1px solid rgba(36, 25, 23, 0.1) !important;
+          border-radius: 0 !important;
+          cursor: pointer !important;
+          transition: all 0.5s ease-in-out !important;
+          min-width: 50px !important;
+          gap: 0.5rem !important;
+        }
+        `)
+        sheet.insertRule(`
+          button:hover {
+            border: 1px solid rgba(36, 25, 23, 1) !important;
+          }
+          `)
+
+          sheet.insertRule(`
+            span[class*='_selectToggleText'] {
+              width: 16px !important;
+            }
+          `);
+
+
+      sheet.insertRule(`
+        div[class*='_selectContainer'] {
+          display: flex !important;
+          align-items: center !important;
+          gap: 0.5rem !important;
+          margin-bottom: 1rem !important;
+        }
+      `);
+
+      sheet.insertRule(`
+        div[class*='_dropdownContainer'] {
           position: absolute !important;
           top: 0 !important;
           left: 0 !important;
-          transform: translateY(-50%) !important;
-          font-size: 0.875rem !important;
+          width: 100% !important;
+          height: 100% !important;
+          background-color: #fff !important;
+          overflow: scroll !important;
         }
-      `)
+      `);
+
+
+        // Errors
+      sheet.insertRule(`
+        output[role=alert] {
+          color: red !important;
+          font-size: 0.8rem !important;
+          margin-top: 0.5rem !important;
+        }
+      `);
+
+     
 
       shadow.adoptedStyleSheets.push(sheet);
 
