dd50819cb9237cbbb9db5406a34a9ba439a66b95
blpoke <84424526+blpoke@users.noreply.github.com>
2025-02-07 13:16:42 +1100

    Final feedback

--- frontend/js/components/HeaderManager.js
+++ frontend/js/components/HeaderManager.js
@@ -30,22 +30,6 @@ export class HeaderManager extends Base {
         this.header.classList.remove("header--not-scrolled");
       }
 
-      // Determine scroll direction
-      if (currentScrollY > this.lastScrollY) {
-        // Scrolling down
-        if (this.header.classList.contains("not-home")) {
-          this.header.classList.add("header--hidden");
-          this.filterBar && this.filterBar.classList.add("header--hidden");
-        } else if (scrollPosition > viewportHeight) {
-          this.header.classList.add("header--hidden");
-          this.filterBar && this.filterBar.classList.add("header--hidden");
-        }
-      } else {
-        // Scrolling up
-        this.header.classList.remove("header--hidden");
-        this.filterBar && this.filterBar.classList.remove("header--hidden");
-      }
-
       const logo = document.getElementById("logo");
 
       if (logo && !this.header.classList.contains("not-home")) {
